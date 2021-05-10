f71b54586daf1929ed03859a1f39fbe8fe6c65b1
tagi17 <naziaozair@gmail.com>
2021-05-10 13:15:49 -0400

    gitadd

--- contracts/tokens.sol
+++ contracts/tokens.sol
@@ -1,10 +1,10 @@
 pragma solidity >=0.6.0 < 0.8.3;
 // SPDX-License-Identifier: MIT
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 
-contract MyToken is ERC20 {
-    constructor () ERC20 ("Penny", "PNY" ){
+contract MyToken is ERC20Capped {
+    constructor () ERC20 ("Penny", "PNY" ) ERC20Capped (100000){
         _mint(msg.sender, 1000);
     }
 }

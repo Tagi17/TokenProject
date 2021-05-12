aaa8aeed3ba65f4615ccaa338e22dfe0e56f4b79
tagi17 <naziaozair@gmail.com>
2021-05-11 21:59:42 -0400

    commit

--- contracts/tokens.sol
+++ contracts/tokens.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.6.0 < 0.8.3;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 
-contract MyToken is ERC20Capped {
-    constructor () ERC20 ("Penny", "PNY" ) ERC20Capped (100000){
-        _mint(msg.sender, 1000);
+contract MyToken is  ERC20Pausable {
+    constructor () ERC20("Penny", "PNY" ) ERC20Capped(100000){
+        ERC20._mint(msg.sender, 1000);
     }
 }
